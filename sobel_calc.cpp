889dc0637df19b1fe4bb76d58c87e8efe9594a2e
Sheden Andemicael <sheden@stanford.edu>
2026-02-06 16:23:19 -0800

    comment out sobelCalc

--- sobel_calc.cpp
+++ sobel_calc.cpp
@@ -34,50 +34,50 @@ void grayScale(Mat& img, Mat& img_gray_out)
  ********************************************/
 void sobelCalc(Mat& img_gray, Mat& img_sobel_out)
 {
-  Mat img_outx = img_gray.clone();
-  Mat img_outy = img_gray.clone();
+  // Mat img_outx = img_gray.clone();
+  // Mat img_outy = img_gray.clone();
 
-  // Apply Sobel filter to black & white image
-  unsigned short sobel;
+  // // Apply Sobel filter to black & white image
+  // unsigned short sobel;
 
-  // Calculate the x convolution
-  for (int i=1; i<img_gray.rows; i++) {
-    for (int j=1; j<img_gray.cols; j++) {
-      sobel = abs(img_gray.data[IMG_WIDTH*(i-1) + (j-1)] -
-		  img_gray.data[IMG_WIDTH*(i+1) + (j-1)] +
-		  2*img_gray.data[IMG_WIDTH*(i-1) + (j)] -
-		  2*img_gray.data[IMG_WIDTH*(i+1) + (j)] +
-		  img_gray.data[IMG_WIDTH*(i-1) + (j+1)] -
-		  img_gray.data[IMG_WIDTH*(i+1) + (j+1)]);
+  // // Calculate the x convolution
+  // for (int i=1; i<img_gray.rows; i++) {
+  //   for (int j=1; j<img_gray.cols; j++) {
+  //     sobel = abs(img_gray.data[IMG_WIDTH*(i-1) + (j-1)] -
+	// 	  img_gray.data[IMG_WIDTH*(i+1) + (j-1)] +
+	// 	  2*img_gray.data[IMG_WIDTH*(i-1) + (j)] -
+	// 	  2*img_gray.data[IMG_WIDTH*(i+1) + (j)] +
+	// 	  img_gray.data[IMG_WIDTH*(i-1) + (j+1)] -
+	// 	  img_gray.data[IMG_WIDTH*(i+1) + (j+1)]);
 
-      sobel = (sobel > 255) ? 255 : sobel;
-      img_outx.data[IMG_WIDTH*(i) + (j)] = sobel;
-    }
-  }
+  //     sobel = (sobel > 255) ? 255 : sobel;
+  //     img_outx.data[IMG_WIDTH*(i) + (j)] = sobel;
+  //   }
+  // }
 
-  // Calc the y convolution
-  for (int i=1; i<img_gray.rows; i++) {
-    for (int j=1; j<img_gray.cols; j++) {
-     sobel = abs(img_gray.data[IMG_WIDTH*(i-1) + (j-1)] -
-		   img_gray.data[IMG_WIDTH*(i-1) + (j+1)] +
-		   2*img_gray.data[IMG_WIDTH*(i) + (j-1)] -
-		   2*img_gray.data[IMG_WIDTH*(i) + (j+1)] +
-		   img_gray.data[IMG_WIDTH*(i+1) + (j-1)] -
-		   img_gray.data[IMG_WIDTH*(i+1) + (j+1)]);
+  // // Calc the y convolution
+  // for (int i=1; i<img_gray.rows; i++) {
+  //   for (int j=1; j<img_gray.cols; j++) {
+  //    sobel = abs(img_gray.data[IMG_WIDTH*(i-1) + (j-1)] -
+	// 	   img_gray.data[IMG_WIDTH*(i-1) + (j+1)] +
+	// 	   2*img_gray.data[IMG_WIDTH*(i) + (j-1)] -
+	// 	   2*img_gray.data[IMG_WIDTH*(i) + (j+1)] +
+	// 	   img_gray.data[IMG_WIDTH*(i+1) + (j-1)] -
+	// 	   img_gray.data[IMG_WIDTH*(i+1) + (j+1)]);
 
-     sobel = (sobel > 255) ? 255 : sobel;
+  //    sobel = (sobel > 255) ? 255 : sobel;
 
-     img_outy.data[IMG_WIDTH*(i) + j] = sobel;
-    }
-  }
+  //    img_outy.data[IMG_WIDTH*(i) + j] = sobel;
+  //   }
+  // }
 
-  // Combine the two convolutions into the output image
-  for (int i=1; i<img_gray.rows; i++) {
-    for (int j=1; j<img_gray.cols; j++) {
-      sobel = img_outx.data[IMG_WIDTH*(i) + j] +
-	img_outy.data[IMG_WIDTH*(i) + j];
-      sobel = (sobel > 255) ? 255 : sobel;
-      img_sobel_out.data[IMG_WIDTH*(i) + j] = sobel;
-    }
-  }
+  // // Combine the two convolutions into the output image
+  // for (int i=1; i<img_gray.rows; i++) {
+  //   for (int j=1; j<img_gray.cols; j++) {
+  //     sobel = img_outx.data[IMG_WIDTH*(i) + j] +
+	// img_outy.data[IMG_WIDTH*(i) + j];
+  //     sobel = (sobel > 255) ? 255 : sobel;
+  //     img_sobel_out.data[IMG_WIDTH*(i) + j] = sobel;
+  //   }
+  // }
 }
